03282710ca85d145979436e4465d1389d0788fdf
Geonnathan <69685807+Geonnathan@users.noreply.github.com>
2021-12-03 11:40:08 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  backend: `http://grupodbackend-env.eba-mms6x9vk.us-east-2.elasticbeanstalk.com`
+  backend: `https://grupod.if60002021.net`
 };
 
 /*
